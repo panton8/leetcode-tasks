addad037d2eca6ba6e27cc9ff9ce201a70056c93
Anton <apodvalnikov@softeqlab.com>
2024-08-02 23:52:34 +0300

    Add tasks: 237, 83

---
+++ 83.py
@@ -0,0 +1,27 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return head
+
+        prev, curr = head, head.next
+
+        while curr:
+            if prev.val == curr.val:
+                prev.next = curr.next
+            else:
+                prev = prev.next
+            curr = curr.next
+
+        return head
+    
